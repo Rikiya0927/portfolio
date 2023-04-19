63ba4b35c1a4f4f0ed042ee5ca60b494506fa870
Rikiya0927 <109444297+Rikiya0927@users.noreply.github.com>
2023-04-19 18:32:55 +0900

    Add files via upload

---
+++ loading.js
@@ -0,0 +1,15 @@
+// loading画面を表示する
+function showLoadingScreen() {
+    document.getElementById("loading").style.display = "flex";
+  }
+  
+  // loading画面を非表示にする
+  function hideLoadingScreen() {
+    document.getElementById("loading").style.display = "none";
+  }
+  
+  // ページの読み込みが完了したらloading画面を非表示にする
+  window.addEventListener("load", function() {
+    hideLoadingScreen();
+  });
+  
