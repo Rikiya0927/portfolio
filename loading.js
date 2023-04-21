14644099d4db929c81fe4ff607905a41a3aedc0f
Rikiya0927 <109444297+Rikiya0927@users.noreply.github.com>
2023-04-21 12:37:46 +0900

    Update loading.js

--- loading.js
+++ loading.js
@@ -8,16 +8,10 @@ function hideLoadingScreen() {
     document.getElementById("load").style.display = "none";
 }
 
-// ページの読み込みが完了したらloading画面を非表示にする
-// window.addEventListener("load", function() {
-//     // 2.5秒後にloading画面を非表示にする
-//     setTimeout(function() {
-//         hideLoadingScreen();
-//     }, 2500);
-// });
-
-// setTimeout(function() {
-//     hideLoadingScreen();
-// }, 2500);
-
-
+ページの読み込みが完了したらloading画面を非表示にする
+window.addEventListener("load", function() {
+    // 2.5秒後にloading画面を非表示にする
+    setTimeout(function() {
+        hideLoadingScreen();
+    }, 2500);
+});
